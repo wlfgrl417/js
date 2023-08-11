426c2b4d3f170732554822d68a3a375086345b06
GABRIEL DE OLIVEIRA SEBRÃO <gabriel_sebrao@estudante.sesisenai.org.br>
2023-08-11 09:54:07 -0300

    commitado

--- intro.js
+++ intro.js
@@ -1,7 +1,7 @@
 const varGlobal = "Sou uma varival global"
 
 function mostrarCode() {
-    let variocal = "sou uma variavel local"
+    let varLocal = "sou uma variavel local"
 
     console.log(varGlobal)
     console.log(varLocal)
